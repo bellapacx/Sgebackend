a00d87fce3541752a156287d132c781bb7928c7f
abel <bellpacx@gmail.com>
2024-09-19 14:51:57 +0300

    Fixhhddvgjddddv

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 const express = require('express');
+//import mongoose from 'mongoose';
 const mongoose = require('mongoose');
-const cors = require('cors');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-
-const productRoutes = require('./routes/productRoutes');
-const storeRoutes = require('./routes/storeRoutes');
-const poRoutes = require('./routes/poRoutes');
-const soRoutes = require('./routes/soRoutes');
-const vehicleRoutes = require('./routes/vehicleRoutes');
-const srRoutes = require('./routes/srRoutes');
-const emptyRoute = require('./routes/emptyRoute');
-const userRoute = require('./routes/userRoute');
+import cors from 'cors';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import productRoutes from './routes/productRoutes';
+import storeRoutes from './routes/storeRoutes';
+import poRoutes from './routes/poRoutes';
+import soRoutes from './routes/soRoutes';
+import vehicleRoutes from './routes/vehicleRoutes';
+import srRoutes from './routes/srRoutes';
+import emptyRoute from './routes/emptyRoute';
+import userRoute from './routes/userRoute';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 5010;
 
 const allowedOrigins = ['https://bellapacx.github.io']; // Add other origins if needed
-
 // Middleware
+// CORS Middleware
 app.use(cors({
   origin: allowedOrigins,
   credentials: true
@@ -26,6 +27,11 @@ app.use(cors({
 
 app.use(express.json());
 app.use(bodyParser.json());
+// Configure allowed origins
+
+
+// Configure MongoDB session store
+const mongoUrl = 'mongodb+srv://bella:bellamongo@cluster0.n3ihytm.mongodb.net/sge?retryWrites=true&w=majority&appName=Cluster0'; // Update to your MongoDB URL
 
 // Configure express-session
 app.use(session({
@@ -33,9 +39,10 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false, // Set to true when serving over HTTPS
+    secure: true, // Use true when serving over HTTPS (your case with GitHub Pages)
     httpOnly: true,
-    sameSite: 'lax' // Use 'lax' or 'strict' based on your needs
+    sameSite: 'none', // Important for cross-origin requests
+    domain: 'bellapacx.github.io',
   }
 }));
 
@@ -43,7 +50,6 @@ app.use(session({
 app.use('/api', productRoutes, storeRoutes, poRoutes, soRoutes, vehicleRoutes, srRoutes, emptyRoute, userRoute);
 
 // Connect to MongoDB
-const mongoUrl = 'mongodb+srv://bella:bellamongo@cluster0.n3ihytm.mongodb.net/sge?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoUrl)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error: any) => console.error('Could not connect to MongoDB:', error));
